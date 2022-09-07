67d1b6fac697f898647fdc76d3bf33b2d7332f05
Alexey Zarubin <alzarubin@gmail.com>
2022-09-07 13:43:26 +0300

    add drop the table to the Main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,17 +7,27 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 public class Main {
     public static void main(String[] args) {
 
+        
         UserService userService = new UserServiceImpl();
+        // create table
         userService.createUsersTable();
-
+        
+        // add 4 users
         userService.saveUser("Vasya", "Pupkin", (byte) 25);
         userService.saveUser("Petya", "Ivanov", (byte) 30);
         userService.saveUser("Sasha", "Sidorov", (byte) 35);
         userService.saveUser("Masha", "Petrova", (byte) 40);
 
+        // get all users and print them out
         System.out.println(userService.getAllUsers());
 
+        // clean table
         userService.cleanUsersTable();
+
+        // drop table
+        userService.dropUsersTable();
+
+
 
 
     }
